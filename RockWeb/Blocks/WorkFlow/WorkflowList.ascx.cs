9a3ea9e23761005d206062064ec17503bf4b8a50
Allen Prell <allen.prell@liveglobal.org>
2021-08-04 12:35:12 -0500

    added WorkflowId as possible context parameter

--- RockWeb/Blocks/WorkFlow/WorkflowList.ascx.cs
+++ RockWeb/Blocks/WorkFlow/WorkflowList.ascx.cs
@@ -356,6 +356,7 @@ namespace RockWeb.Blocks.WorkFlow
                     var qryParam = new Dictionary<string, string>();
                     qryParam.Add( "WorkflowTypeId", workflow.WorkflowTypeId.ToString() );
                     qryParam.Add( "WorkflowGuid", workflow.Guid.ToString() );
+                    qryParam.Add( "WorkflowId", workflow.Id.ToString() );
                     NavigateToLinkedPage( "EntryPage", qryParam );
                 }
                 else
@@ -746,3 +747,4 @@ namespace RockWeb.Blocks.WorkFlow
         #endregion
     }
 }
+
