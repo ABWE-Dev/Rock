f1783b008c19b0e69e7339442d3d5983036fe23a
Luke Bennett <programmerbygrace@gmail.com>
2023-10-24 15:09:59 -0400

    Universal Search/Content Collection Improvements + Added slug to Content Collection content channel items + Made attributes searchable in Content Collections + Made ContentChannelItem attributes searchable in universal search

--- Rock/Cms/ContentCollection/Indexers/ContentChannelItemIndexer.cs
+++ Rock/Cms/ContentCollection/Indexers/ContentChannelItemIndexer.cs
@@ -143,7 +143,7 @@ namespace Rock.Cms.ContentCollection.Indexers
 
             using ( var rockContext = new RockContext() )
             {
-                itemEntity = new ContentChannelItemService( rockContext ).Get( id );
+                itemEntity = new ContentChannelItemService( rockContext ).GetInclude(id, (i) => i.ContentChannelItemSlugs);
                 var now = RockDateTime.Now;
 
                 // If entity wasn't found or isn't visible yet then don't index.
